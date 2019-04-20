ec76b825d79dad446e415390281ff20b64c550a3
Ross Findlay <r.findlay.05@aberdeen.ac.uk>
2019-04-20 09:46:05 +0300

    Updates

---
+++ precache-manifest.358e40a753f917e3bd3313511163e302.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "796bdeb29a209f20255d",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "796bdeb29a209f20255d",
+    "url": "/contactmanager/static/js/main.12cf8f44.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "e12419c6a9c32f2309f5",
+    "url": "/contactmanager/static/css/2.5bc1895a.chunk.css"
+  },
+  {
+    "revision": "e12419c6a9c32f2309f5",
+    "url": "/contactmanager/static/js/2.a46c82bc.chunk.js"
+  },
+  {
+    "revision": "a44cd9b2715aa05b9ca744283b2d2642",
+    "url": "/contactmanager/index.html"
+  }
+];
